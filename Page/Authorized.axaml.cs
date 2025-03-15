ff9882fe0717d344180668921bd31f421be00426
Rvmzesbro <galeevramazan64@gmail.com>
2025-03-15 15:05:54 +0300

    four1

--- Page/Authorized.axaml.cs
+++ Page/Authorized.axaml.cs
@@ -1,13 +1,18 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using FirstAppAvalonia.Models;
 
 namespace FirstAppAvalonia.Page;
 
 public partial class Authorized : UserControl
 {
+    public User ContextUser { get; set; }
     public Authorized()
     {
         InitializeComponent();
+        
     }
+    
 }
